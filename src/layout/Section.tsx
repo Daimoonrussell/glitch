d4683aa0163f13ef027c5074d6e431c4faea4ad7
Daimoonrussell <daimoonrussell@gmail.com>
2025-05-25 22:52:33 +0200

    commit: push na GitHub

--- src/layout/Section.tsx
+++ src/layout/Section.tsx
@@ -1,4 +1,6 @@
+import { motion } from 'framer-motion';
 import type { ReactNode } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 type ISectionProps = {
   title?: string;
@@ -7,25 +9,36 @@ type ISectionProps = {
   children: ReactNode;
 };
 
-const Section = (props: ISectionProps) => (
-  <div
-    className={`mx-auto max-w-screen-lg px-3 ${
-      props.yPadding ? props.yPadding : 'py-16'
-    }`}
-  >
-    {(props.title || props.description) && (
-      <div className="mb-12 text-center">
-        {props.title && (
-          <h2 className="text-4xl font-bold text-white">{props.title}</h2>
-        )}
-        {props.description && (
-          <div className="mt-4 text-xl md:px-20">{props.description}</div>
-        )}
-      </div>
-    )}
+const Section = (props: ISectionProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.15,
+  });
 
-    {props.children}
-  </div>
-);
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 64 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.7, ease: 'easeOut' }}
+      className={`mx-auto max-w-screen-lg px-3 ${
+        props.yPadding ? props.yPadding : 'py-16'
+      }`}
+    >
+      {(props.title || props.description) && (
+        <div className="mb-12 text-center">
+          {props.title && (
+            <h2 className="text-4xl font-bold text-white">{props.title}</h2>
+          )}
+          {props.description && (
+            <div className="mt-4 text-xl md:px-20">{props.description}</div>
+          )}
+        </div>
+      )}
+
+      {props.children}
+    </motion.div>
+  );
+};
 
 export { Section };
